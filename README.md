5ac1ebb4d3fbed849ee5adc1f06c3f641ef0ff67
waracci <morriswarachi0@gmail.com>
2017-06-11 05:46:58 +0300

    initialized repo and added the main features of the flash card application

---
+++ README.md
@@ -0,0 +1,2 @@
+# Author: Waracci
+# instructions, clone the repo and run the index.html file in your prefeered browser.
---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $('#flashBtn1').click(function () {
+        $('#flash1').toggle();
+        $('#flashAns1').toggle();
+    });
+    $('#flashBtn2').click(function () {
+        $('#flash2').toggle();
+        $('#flashAns2').toggle();
+    });
+    $('#flashBtn3').click(function () {
+        $('#flash3').toggle();
+        $('#flashAns3').toggle();
+    });
+    $('#flashBtn12').click(function () {
+        $('#flash12').toggle();
+        $('#flashAns12').toggle();
+    });
+    $('#flashBtn22').click(function () {
+        $('#flash22').toggle();
+        $('#flashAns22').toggle();
+    });
+    $('#flashBtn32').click(function () {
+        $('#flash32').toggle();
+        $('#flashAns32').toggle();
+    });
+    $('#flashBtn13').click(function () {
+        $('#flash13').toggle();
+        $('#flashAns13').toggle();
+    });
+    $('#flashBtn23').click(function () {
+        $('#flash23').toggle();
+        $('#flashAns23').toggle();
+    });
+    $('#flashBtn33').click(function () {
+        $('#flash33').toggle();
+        $('#flashAns33').toggle();
+    });
+    $('#flashBtn14').click(function () {
+        $('#flash14').toggle();
+        $('#flashAns14').toggle();
+    });
+    $('#flashBtn24').click(function () {
+        $('#flash24').toggle();
+        $('#flashAns24').toggle();
+    });
+    $('#flashBtn34').click(function () {
+        $('#flash34').toggle();
+        $('#flashAns34').toggle();
+    });
+});
